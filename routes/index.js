9b8167d30d543abf1864520f954c7f02d0f0f8ad
The-isaacagboola <isaacadeyemi9@gmail.com>
2025-03-03 10:05:08 +0100

    Fixes for deployment

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+// All authours
 router.get("/", (req, res) => {
   res.render("index");
 });
 
+// New  Author Route
 module.exports = router;
